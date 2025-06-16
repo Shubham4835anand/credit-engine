0a64f93ba2326cee356db111c1fa78faa7fed9b4
Shubham Anand <109297009+Shubham4835anand@users.noreply.github.com>
2025-06-16 20:52:16 +0530

    index.js

--- index.js
+++ index.js
@@ -11,5 +11,11 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', creditRoutes);
 
+app.get('/', (req, res) => {
+  res.redirect('/enroll.html');
+});
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running at http://localhost:${PORT}`)
+);
